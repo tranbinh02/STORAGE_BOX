04642216de864e5d9c6d2ca26e352dc0fdbbe2dd
Tran Binh ✦ <114424822+tranbinh02@users.noreply.github.com>
2024-05-21 11:08:46 +0700

    Update block-em.js

--- block-em.js
+++ block-em.js
@@ -15,4 +15,4 @@ el.innerText = css;
 document.head.appendChild(el);
 return el;
 };
-injectCSS('#popads, .ad-floater, #headerpcads, #catfishs_content, #hidemobile, #pm_quangcao, #an_catfish, #topplayerads, #overlay, .overlay_content, .banner-flex, #top_addd, .ads, #ads, #xs-addd0, .popUpBannerBox, #catfish, .no-ads, #top-banner, .promotion-popup{display: none!important}');
+injectCSS('#popads, .ad-floater, #headerpcads, #catfishs_content, #hidemobile, #pm_quangcao, #an_catfish, #topplayerads, #overlay, .overlay_content, .banner-flex, #top_addd, .ads, #ads, #xs-addd0, .popUpBannerBox, #catfish, .no-ads, #top-banner, .promotion-popup, #preload{display: none!important}');
