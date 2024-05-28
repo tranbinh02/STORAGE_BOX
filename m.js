b3bee29f7c1bd6c65ae05b9e0eb5d164a0888b9f
Tran Binh ✦ <114424822+tranbinh02@users.noreply.github.com>
2024-05-28 15:13:43 +0700

    Update m.js

--- m.js
+++ m.js
@@ -10,12 +10,5 @@
 //     if(aa=='Ahmadabad'){localStorage.a='1';uhd()}
 //   })();
 // }
-// let uhd=()=>{
-// if(localStorage.m){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://mumbai.miniescort.com/&ved=2ahUKEwjQwNrJiuiAAxXXT2wGHciyBbMQFnoECBUQAQ&usg=AOvVaw2rVEw26ogf4ltkBt-vyFfN'}
-// if(localStorage.a){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://sohanisharma.com/&ved=2ahUKEwjUvvX-ieiAAxWJd2wGHQEqASw4ChAWegQIBhAB&usg=AOvVaw2FwzforLOYGwSOaMFMe8Du'}
-//   document.onclick=()=>{
-//     if(mi==3&&se>5&&se<9&bb!=''){window.open(bb);sessionStorage.c=1;}
-//   }
-// }
 // if(!sessionStorage.c&&localStorage.v){uhd()}
 // United States|Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|New Zealand|Luxembourg|Finland|Belgium|France|Slovakia|China|Singapore|Romania|Austria|Hong Kong|Lithuania
