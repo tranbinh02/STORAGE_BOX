ce9e7a726d6ca8e9a0790d49e5b866a3101e2cd1
Tran Binh ✦ <114424822+tranbinh02@users.noreply.github.com>
2024-06-21 16:44:24 +0700

    Create lightboxcog.js

---
+++ lightboxcog.js
@@ -0,0 +1,17 @@
+Fancybox.bind('[data-src]', {
+        Toolbar: {
+          display: {
+            left: ["infobar"],
+            middle: [
+              "zoomIn",
+              "zoomOut",
+              "toggle1to1",
+              "rotateCCW",
+              "rotateCW",
+              "flipX",
+              "flipY",
+            ],
+            right: ["slideshow", "download", "thumbs", "fullscreen", "close"],
+          },
+        },
+});    
