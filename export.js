862b620b041e221a90b83163774f9e755b195587
Tran Binh ✦ <114424822+tranbinh02@users.noreply.github.com>
2024-06-30 22:52:59 +0700

    Update export.js

--- export.js
+++ export.js
@@ -2,7 +2,7 @@
 // @name         Export Image URLs (2024)
 // @version      0.0.2
 // @description  Extracts image URLs from a web page and copies them to the clipboard when a button is clicked.
-// @author       tranbinh, Origin: sharmanhall
+// @author       tranbinh02, origin: sharmanhall
 // @supportURL   *
 // @namespace    *
 // @license      MIT
