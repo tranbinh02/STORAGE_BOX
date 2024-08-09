dd535d83c120685ab337799b9f8b3f89a4dbc921
Tran Binh ✦ <114424822+tranbinh02@users.noreply.github.com>
2024-08-09 23:41:33 +0700

    Update block-em.js

--- block-em.js
+++ block-em.js
@@ -24,6 +24,7 @@
 
 (function() {
     'use strict';
+
     const elementsToRemove = [
         '.ad-container',
         '.watch-banner-2',
@@ -35,21 +36,41 @@
         '.ad-floater',
         '.no-ads',
         '.popUpBannerBox',
-        '#catfish',
-        '',
-        '',
-        '',
-        '',
-        '',
-        '',
+        '#catfish'
     ];
-    function removeElements() {
-        elementsToRemove.forEach(selector => {
+
+    const observer = new MutationObserver((mutations) => {
+        const elementsFound = new Set();
+        
+        for (const mutation of mutations) {
+            for (const node of mutation.addedNodes) {
+                if (node.nodeType === Node.ELEMENT_NODE) {
+                    for (const selector of elementsToRemove) {
+                        if (node.matches(selector) || node.querySelector(selector)) {
+                            elementsFound.add(selector);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (elementsFound.size > 0) {
+            removeElements(Array.from(elementsFound));
+        }
+    });
+
+    function removeElements(selectors) {
+        const fragment = document.createDocumentFragment();
+        for (const selector of selectors) {
             const elements = document.querySelectorAll(selector);
-            elements.forEach(element => {
-                element.remove();
-            });
-        });
+            elements.forEach(element => fragment.appendChild(element));
+        }
+        fragment.textContent = '';
     }
-    removeElements();
+
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    // Initial removal
+    removeElements(elementsToRemove);
 })();
