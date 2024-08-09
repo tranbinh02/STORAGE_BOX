9ebc165d5c171d0f4787b67ef8661979af421fd3
Tran Binh ✦ <114424822+tranbinh02@users.noreply.github.com>
2024-08-09 23:26:35 +0700

    Create getimage_speed.js

---
+++ getimage_speed.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Get image speed
+// @version      0.2
+// @description  Get image speed
+// @author       tranbinh
+// @supportURL   *
+// @namespace    *
+// @license      MIT
+// @connect      greasyfork.org
+// @connect      sleazyfork.org
+// @connect      github.com
+// @connect      openuserjs.org
+// @match        *://*/*
+// @exclude      *://*.youtube.com/*
+// @exclude      *://docs.google.com/
+// @grant        GM_setClipboard
+// @grant        GM_log
+// @compatible   chrome
+// @compatible   firefox
+// @compatible   edge
+// @compatible   opera
+// @compatible   safari
+// @run-at       document-idle
+// @icon         *
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function handleContextMenu(e) {
+        let imageUrl = null;
+        let target = e.target;
+        while (target && !imageUrl) {
+            if (target.tagName && target.tagName.toLowerCase() === 'img') {
+                imageUrl = target.src;
+            } else {
+                const computedStyle = window.getComputedStyle(target);
+                const backgroundImage = computedStyle.getPropertyValue('background-image');
+                if (backgroundImage !== 'none') {
+                    imageUrl = backgroundImage.slice(4, -1).replace(/['"]/g, "");
+                }
+            }
+            target = target.parentElement;
+        }
+        if (imageUrl) {
+            e.preventDefault();
+            window.open(imageUrl, '_blank');
+        }
+    }
+
+    if (document.addEventListener) {
+        document.addEventListener('contextmenu', handleContextMenu, false);
+    } else if (document.attachEvent) {
+        document.attachEvent('oncontextmenu', handleContextMenu);
+    }
+})();
